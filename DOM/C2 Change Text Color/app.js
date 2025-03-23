4b60a13a2bbc8670473269cc896361a00638592b
vise07cs <vikram7cs@gmail.com>
2025-03-23 17:32:13 +0530

    C2

---
+++ DOM/C2 Change Text Color/app.js
@@ -0,0 +1,24 @@
+const heading=document.getElementById("mainHeading")
+// console.log(heading)
+// heading.style.color="blue";
+
+
+function redButton(){
+  heading.style.color="red";
+}
+
+function greenButton(){
+  heading.style.color="green";
+}
+
+function blueButton(){
+  heading.style.color="blue";
+}
+
+function purpleButton(){
+  heading.style.color="purple";
+}
+
+function resetButton(){
+  heading.style.color="black";
+}
---
+++ DOM/C2 Change Text Color/readme.md
@@ -0,0 +1,9 @@
+## Challenge 2: Change Text Color 🦎
+
+- Create a webpage with a heading that says "Change My Color!"
+- Add five buttons: Red, Green, Blue, Purple (or any four colors of your choice) and Reset.
+- When a color button is clicked, the heading’s text color should change to the corresponding color.
+- Clicking the Reset button should restore the text color to black .
+
+
+
